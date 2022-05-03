0bd738e9a8f0e3db6cb173829708d7f912d24e57
Emilien31 <59746560+Emilien31@users.noreply.github.com>
2022-05-03 11:52:52 +0200

    🚸 Add an example code

---
+++ test.py
@@ -0,0 +1,13 @@
+import receive_sms # Import the module
+
+print('version: '+receive_sms.version()) # Print the version
+
+data = receive_sms.get_messages(number_phone = '12018577757') # Get messages sent to 12018577757
+
+# Print informations of each message
+for msg in data:
+    print(msg.text) # The content of the message
+    print('from '+msg.from_number) # The number from which the message originated
+    print('to '+msg.to_number) # Your number, in this case it's 12018577757
+    print('the '+msg.date.strftime("%m/%d/%Y, %H:%M")) # The date
+    print('\n') # Line break
